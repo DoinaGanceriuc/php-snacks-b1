9d05b006a8ae08011f7bc51552b1eee5a2be881f
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-12-02 12:15:57 +0100

    add Snack 1

---
+++ Snack 1/index.php
@@ -0,0 +1,54 @@
+<!-- Snack 1:
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
+Olimpia Milano - Cantù | 55-60 -->
+
+<?php
+
+
+$partite = [
+    [
+    'squadraCasa' => 'Olimpia Milano',
+    'puntiCasa' => 55
+    ],
+    [
+    'squadraOspite' => 'Cantù',
+    'puntiOspite' => 60
+    ]    
+];
+/* var_dump($partite[0], $partite[1]);
+echo $partite['squadraCasa']; */
+
+/* for ($i=0; $i < count($partite); $i++) { 
+   $element = $partite[$i];
+   var_dump($element['squadraCasa']);
+   echo $element['squadraOspite'];
+} */
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partite</title>
+</head>
+<body>
+<h2>Partita</h2>
+
+<?php
+for ($i=0; $i < count($partite); $i++) {
+    $element = $partite[$i];?>
+
+        <p><?php echo $element['squadraCasa'],$element['squadraOspite'] ?></p>
+    
+    
+
+
+<?php }
+?>
+    
+</body>
+</html>
